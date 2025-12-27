79b9fb61391bd4f7ccb7e06fa4efd54f44a6ded9
Ondra Beneš <ondra.benes@gmail.com>
2025-12-27 18:11:08 +0100

    Simplify run script

--- run.sh
+++ run.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-VENV=".venv"
-SCRIPT="${1:-mixdown.py}"
-
-python3 -m venv "$VENV"
-source "$VENV/bin/activate"
-
-pip install --upgrade pip
-pip install -r requirements.txt
-
-python "$SCRIPT"
+python3 -m venv .venv
+source .venv/bin/activate
+python -m pip install -r requirements.txt
+python mixdown.py
